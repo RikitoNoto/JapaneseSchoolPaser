1bccf2d8fdc56bc891915abde2d025eec08abe05
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-10-24 23:00:07 +0900

    学校名を追加

--- src/models/base_info.py
+++ src/models/base_info.py
@@ -2,9 +2,14 @@ from models.model import Model
 
 
 class BaseInfo(Model):
-    def __init__(self, school_code="", president="") -> None:
+    def __init__(self, name="", school_code="", president="") -> None:
+        self.__name = name
         self.__school_code: str = school_code
         self.__president: str = president
+
+    @property
+    def name(self) -> str:
+        return self.__name
 
     @property
     def school_code(self) -> str:
--- src/parser/base_info_parser.py
+++ src/parser/base_info_parser.py
@@ -8,6 +8,8 @@ class BaseInfoParser(Parser):
     def parse(self) -> BaseInfo:
         """
         学校コードのセルを基準にデータを検索しパースする。
+        それに加えて、ファイル名とB1セルから大学名と学校タイプをパースする。
+        学校コードが見つからない場合は、情報が取得できないためValueErrorをraiseする。
         """
         base_cell: Optional[Cell] = self._search_cell(
             "学校コード",
@@ -15,7 +17,11 @@ class BaseInfoParser(Parser):
         )
         if not base_cell:
             raise ValueError("学校コードが見つかりませんでした。")
+
+        name = self._sheet.title  # シート名が大学名
+
         return BaseInfo(
+            name=name,
             school_code=self._sheet.cell(
                 row=base_cell.row + 1, column=base_cell.column
             ).value,
